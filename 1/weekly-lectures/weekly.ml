fd3ba9231d558421b1ffce5f593591b674b356ba
EadyJj17 <168749878+EadyJj17@users.noreply.github.com>
2024-10-18 09:22:46 +0000

    Soluzione Weekly

--- 1/weekly-lectures/weekly.ml
+++ 1/weekly-lectures/weekly.ml
@@ -11,4 +11,18 @@ let isLecture (d : weekday) (c : course) : bool =
       if (d == We || d == Th) then
         true
       else false
-    else false
+    else false;;
+
+(*ALF*)
+assert(isLecture Mo ALF = false);;
+assert(isLecture Tu ALF = true);;
+assert(isLecture We ALF = false);;
+assert(isLecture Th ALF = true);;
+assert(isLecture Fr ALF = true);;
+
+(*LIP*)
+assert(isLecture Mo LIP = false);;
+assert(isLecture Tu LIP = false);;
+assert(isLecture We LIP = true);;
+assert(isLecture Th LIP = true);;
+assert(isLecture Mo LIP = false);;
