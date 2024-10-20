67a4c6e2fe9375279d6f16b9d2246cfb790344a0
EadyJj17 <168749878+EadyJj17@users.noreply.github.com>
2024-10-20 21:20:59 +0000

    Soluzione Movie Ratings

---
+++ 1/movie-ratings/movieRatings.ml
@@ -0,0 +1,25 @@
+let movie_rating (r1 : int) (r2 : int) (r3 : int) : string =
+  if (r1 > 5 || r1 < 1 || r2 > 5 || r2 < 1 || r3 > 5 || r3 < 1) then
+    "Errore"
+  else if (r1 == 5 && r2 == 5 && r3 == 5) then
+    "Masterpiece"
+  else if (r1 == 5 && r2 == 5 && r3 == 4
+          || r1 == 5 && r2 == 4 && r3 == 5
+          || r1 == 4 && r2 == 5 && r3 == 5) then
+    "Highly Recommended"
+  else if (r1 >= 4 && r2 >= 4 && r3 >= 3 && r3 <= 5) then
+    "Recommended"
+  else
+    "Mixed Reviews"
+
+let cont
+let movie_rating2 (r1 : int) (r2 : int) (r3 : int) : string =
+
+
+    assert(movie_rating 5 5 5 = "Masterpiece");;
+    assert(movie_rating 5 5 4 = "Highly Recommended");;
+    assert(movie_rating 5 4 5 = "Highly Recommended");;
+    assert(movie_rating 4 5 5 = "Highly Recommended");;
+    assert(movie_rating 5 4 4 = "Recommended");;
+    assert(movie_rating 4 4 4 = "Recommended");;
+    assert(movie_rating 4 4 3 = "Recommended");;
